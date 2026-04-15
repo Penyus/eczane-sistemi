56c36d784af6988ae7a372fc66fdb72a987e0c9e
Yarengulungor <yarengulungor@stu.aydin.edu.tr>
2026-04-15 22:01:57 +0300

    Update apps.py
    Added default_auto_field and ready method to StokConfig.

--- stok/apps.py
+++ stok/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class StokConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'stok'
+
+    def ready(self):
+        import stok.signals
