bd2f1a2546752e0509e984dc332c7c9467af5f2f
Dovlet Seyidov <dovletseyidov1007@gmail.com>
2026-04-30 03:35:06 +0300

    feat(admin): stok modelleri admin paneline eklendi

--- stok/admin.py
+++ stok/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ilac, StokHareket, Recete, ReceteIlac, Kategori
 
-# Register your models here.
+admin.site.register(Kategori)
+admin.site.register(Ilac)
+admin.site.register(StokHareket)
+admin.site.register(Recete)
+admin.site.register(ReceteIlac)
